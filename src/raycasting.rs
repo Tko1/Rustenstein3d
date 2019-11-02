fc46154a255c06f09ebf5329a372ba5814953a8a
Cameron Cooper <zdot101@gmail.com>
2019-11-02 12:51:11 -0400

    Begin building raycasting, to possibly be divided up into other modules later. Starting with Camera

--- src/raycasting.rs
+++ src/raycasting.rs
@@ -1,3 +1,13 @@
 use crate::math::Vec2f;
-}
+use crate::math::Angle;
 
+pub struct Transform(pub Vec2f);
+ 
+struct Camera {
+    transform: Transform,
+    rotation: Angle,
+    horizontal_view_angle: Angle,
+    vertical_view_angle: Angle,
+    //Basically, how many pixels wide this view is, and therefore how many rays we need to cast
+    view_width: i32
+}
