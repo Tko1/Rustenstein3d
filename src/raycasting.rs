a1f56e8e7243b14917c17524a0e138f37f9e3473
Cameron Cooper <zdot101@gmail.com>
2019-11-02 12:50:44 -0400

    Move angle to math

--- src/raycasting.rs
+++ src/raycasting.rs
@@ -1,24 +1,3 @@
 use crate::math::Vec2f;
-
-#[derive(Debug,Copy,Clone)]
-pub struct Angle(pub Vec2f);
-
-impl Angle {
-    pub fn new(x:f32, y:f32) -> Angle { Angle(Vec2f::new(x,y)) }
-    pub fn get_vec(&self) -> Vec2f { self.0 }
-    pub fn get_rad(&self) -> f32
-    {
-        let Vec2f {x: x, y: y} = self.get_vec();
-        return y.atan2(x);
-    }
-}
-
-pub trait ToAngle {
-    fn to_angle(&self) -> Angle;
-}
-impl ToAngle for f32 {
-    fn to_angle(&self) -> Angle {
-        Angle(Vec2f::new(self.cos(),self.sin()))      
-    }
 }
 
