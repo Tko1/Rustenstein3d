84a59c479b31cb8cff732dc4557ad08a9ce1d325
Cameron Cooper <zdot101@gmail.com>
2019-11-07 02:19:14 -0500

    Next step in the process;  we can now take the ray and detect each tile collision it makes,  so we are going to keep crossing tiles until we have a collision, then we will see how far the ray went and get its length

--- src/raycasting.rs
+++ src/raycasting.rs
@@ -69,5 +69,64 @@ impl Camera {
         let Transform(pos) = self.transform;
 
 	
+	// We are flat out using the ol mx + y0 = y to represent our ray on the map
+        // This is just the inverse function, to get x given a y 
+        let y_to_x = |ray_slope,pos,y| {
+	    let Vec2f {x: px, y: py} = pos;
+	    // y intercept; See notes, this can be derived with your old mx + y0 = y
+            // and will be used to create a f(x) = y,  and a f(y) = x  
+	    let y0 = py - ray_slope * px;
+	    (y - y0)/ray_slope
+        };
+	
+	// We are flat out using the ol mx + y0 = y to represent our ray on the map
+        // So we can get a y given an x
+        let x_to_y = |ray_slope,pos,x| {
+	    let Vec2f {x: px, y: py} = pos;
+	    // y intercept; See notes, this can be derived with your old mx + y0 = y
+            // and will be used to create a f(x) = y,  and a f(y) = x  
+	    let y0 = py - ray_slope * px;
+	    ray_slope * x + y0
+        };
+        let get_ray_next_tile_cross = |map: Map,pos: Vec2f, angle : Angle| -> Vec2f
+        {
+            
+            let Vec2f {x: px, y: py} = pos;
+            // |     |       <--- y_from_tile (and the overall rectangle this makes in the corner is the rectangle)
+            // |     .----   <--- x_from_tile (and the dot is (px,py))
+            // |_________|   
+            let (x_from_tile,y_from_tile) = (px.ceil() - px, py.ceil() - py);
+            // |     | _ /   <-- rectangle_slope  (a ray from our pos that perfectly exits through our tile corner)
+            // |     /----                        If our ray is above this, it exits through the top
+            // |_________|                        If our ray slopes below this, it exits through the right
+            let rectangle_slope = y_from_tile / x_from_tile;
+            let ray_slope = angle.slope();
+
+	    // This means we will cross at the next y, or py.ceil()
+	    if ray_slope > rectangle_slope {
+		let next_x = y_to_x(ray_slope,pos,py.ceil());
+		return Vec2f::new(next_x,py.ceil());
+            }
+            // We cross at the next x
+            else if ray_slope < rectangle_slope {
+		let next_y = x_to_y(ray_slope,pos,px.ceil());
+		return Vec2f::new(px.ceil(),next_y);
+            }
+            // If ray_slope = rectangle_slope, go to px.ceil(),py.ceil()
+            else { 
+                return Vec2f::new(px.ceil(),py.ceil());
+            }
+
+        };
+	let ray_lengths = ray_angles.into_iter().map(|x| {
+	    
+	}).collect::<Vec<()>>();
+        let mut ray_length = 0;
+	/*
+	for ray_angle in ray_angles {
+	    let ray_slope = ray_angle.slope();	    
+	    
+	}
+	 */
     }
 }
